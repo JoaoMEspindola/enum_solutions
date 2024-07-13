e665d9746aadd88915e98d5f82110ecefc9885bd
Joao <jpmaresp@gmail.com>
2024-07-12 22:42:24 -0300

    added last prints

--- main.py
+++ main.py
@@ -4,5 +4,13 @@ content = dataManager.readFullFile('trabalho_1_PO/assets/exemplo1.txt')
 
 listaSolucoes = dataManager.buildProblem(content)
 
+solucoesViaveis = 0
+solucoesInviaveis = 0
 for solucao in listaSolucoes:
     solucao.toString()
+    if solucao.viabilidade == 'viável':
+        solucoesViaveis += 1
+    else:
+        solucoesInviaveis += 1
+
+print(f'\nSoluções básicas viáveis:   {solucoesViaveis}\nSoluções básicas inviáveis: {solucoesInviaveis}')
